6f78872ef819529a99f4c1c104a8c509bdcf0412
anuD <adilbagi22@gmail.com>
2023-07-13 18:11:15 -0700

    Added questions to prompt

---
+++ main/inquirerQs.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        type: 'input',
+        message: 'View all departments',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'View all roles',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'View all employees',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Add a role',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Add an employee',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Update an employee role',
+        name: 'description'
+    }]
